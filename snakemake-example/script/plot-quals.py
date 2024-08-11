08af633a847ecb26ef4dbdcfc2e539c155197ca4
elena-bio <166811346+elena-bio@users.noreply.github.com>
2024-08-11 23:55:09 +0200

    Delete snakemake-example directory

--- snakemake-example/script/plot-quals.py
+++
@@ -1,9 +0,0 @@
-import matplotlib
-matplotlib.use("Agg")
-import matplotlib.pyplot as plt
-from pysam import VariantFile
-
-quals = [record.qual for record in VariantFile("output/calls/all.vcf")]
-plt.hist(quals)
-
-plt.savefig("output/plots/quals.svg")
